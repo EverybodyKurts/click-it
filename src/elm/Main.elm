31852d3147a50395855bc06f72c0496ebce85241
Kurt Mueller <kurtronaldmueller@gmail.com>
2017-07-07 17:51:23 -0400

    Create simple svg graphics

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,6 +1,8 @@
 module Main exposing (..)
 
 import Html exposing (Html, h1, text)
+import Svg exposing (svg, rect)
+import Svg.Attributes exposing (width, height, viewBox, x, y, rx, ry)
 
 
 -- MODEL
@@ -27,12 +29,13 @@ view : Model -> Html Msg
 view model =
     -- The inline style is being used for example purposes in order to keep this example simple and
     -- avoid loading additional resources. Use a proper stylesheet when building your own app.
-    h1 []
-        [ text "Hello Elm!" ]
+    svg
+        [ width "120", height "120", viewBox "0 0 120 120" ]
+        [ rect [ x "10", y "10", width "100", height "100", rx "15", ry "15" ] [] ]
 
 
 
--- Msg
+-- Message
 
 
 type Msg
