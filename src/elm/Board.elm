c3bc95e30cc8196fc43284eab54a4e82bc5e485d
Kurt Mueller <kurtronaldmueller@gmail.com>
2017-07-08 20:35:49 -0400

    Calculate a board's dimensions

--- src/elm/Board.elm
+++ src/elm/Board.elm
@@ -20,6 +20,43 @@ type alias Board =
     }
 
 
+type PixelHeight
+    = PixelHeight Int
+
+
+type PixelWidth
+    = PixelWidth Int
+
+
 default : Board
 default =
     Board (BoardHeight 10) (BoardWidth 5) (PieceLength 25)
+
+
+pixelWidth : Board -> PixelWidth
+pixelWidth { pieceLength, width } =
+    let
+        (PieceLength pLength) =
+            pieceLength
+
+        (BoardWidth bdWidth) =
+            width
+    in
+        PixelWidth (pLength * bdWidth)
+
+
+pixelHeight : Board -> PixelHeight
+pixelHeight { pieceLength, height } =
+    let
+        (PieceLength pLength) =
+            pieceLength
+
+        (BoardHeight bdHeight) =
+            height
+    in
+        PixelHeight (pLength * bdHeight)
+
+
+dimensions : Board -> ( PixelHeight, PixelWidth )
+dimensions board =
+    ( pixelHeight board, pixelWidth board )
