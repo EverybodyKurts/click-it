d1cc02e628eaa38ec28f85951314848c49203129
Kurt Mueller <kurtronaldmueller@gmail.com>
2017-09-10 13:16:22 -0400

    Get raw value of a boar'ds # of columns & piece length

--- src/elm/Board/Properties.elm
+++ src/elm/Board/Properties.elm
@@ -28,3 +28,25 @@ type alias Properties =
 default : Properties
 default =
     Properties (NumRows 10) (NumColumns 5) (NumColors 3) (PieceLength 25)
+
+
+{-| Get raw value of a board's # of columns
+-}
+numColumnsValue : Properties -> Int
+numColumnsValue { numColumns } =
+    let
+        (NumColumns c) =
+            numColumns
+    in
+        c
+
+
+{-| Get raw value of a board's piece length
+-}
+pieceLengthValue : Properties -> Int
+pieceLengthValue { pieceLength } =
+    let
+        (PieceLength pl) =
+            pieceLength
+    in
+        pl
