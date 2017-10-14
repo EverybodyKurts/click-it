cd86b2d77c5543ac0302a496bb3655b56fd15f28
Kurt Mueller <kurtronaldmueller@gmail.com>
2017-10-14 17:09:25 -0400

    Add functions for updating properties & drawing board based on props

--- src/elm/Board/Properties.elm
+++ src/elm/Board/Properties.elm
@@ -25,14 +25,17 @@ type alias Properties =
     }
 
 
+
+-- INITIALIZING BOARD PROPERTIES
+
+
 default : Properties
 default =
     Properties (NumRows 10) (NumColumns 5) (NumColors 3) (PieceLength 25)
 
 
-numRowsVal : NumRows -> Int
-numRowsVal (NumRows r) =
-    r
+
+-- ACCESSING PROPERTIES
 
 
 raw : Properties -> ( Int, Int, Int, Int )
@@ -51,3 +54,78 @@ raw { numRows, numColumns, numColors, pieceLength } =
             pieceLength
     in
         ( rows, columns, colors, pl )
+
+
+
+-- UPDATING PROPERTIES
+
+
+{-| Update the number of rows
+-}
+updateNumRows : Properties -> NumRows -> Properties
+updateNumRows properties numRows =
+    { properties | numRows = numRows }
+
+
+{-| Update the number of rows from a string, most likely user input.
+-}
+updateNumRowsFromString : Properties -> String -> Result String Properties
+updateNumRowsFromString properties rawNumRows =
+    String.toInt rawNumRows
+        |> Result.map (clamp 1 100)
+        |> Result.map NumRows
+        |> Result.map (updateNumRows properties)
+
+
+updateNumColumns : Properties -> NumColumns -> Properties
+updateNumColumns properties numColumns =
+    { properties | numColumns = numColumns }
+
+
+updateNumColumnsFromString : Properties -> String -> Result String Properties
+updateNumColumnsFromString properties rawNumColumns =
+    String.toInt rawNumColumns
+        |> Result.map (clamp 1 100)
+        |> Result.map NumColumns
+        |> Result.map (updateNumColumns properties)
+
+
+updateNumColors : Properties -> NumColors -> Properties
+updateNumColors properties numColors =
+    { properties | numColors = numColors }
+
+
+updateNumColorsFromString : Properties -> String -> Result String Properties
+updateNumColorsFromString properties rawNumColors =
+    String.toInt rawNumColors
+        |> Result.map (clamp 1 100)
+        |> Result.map NumColors
+        |> Result.map (updateNumColors properties)
+
+
+
+-- DRAWING THE BOARD
+
+
+width : Properties -> Int
+width { numColumns, pieceLength } =
+    let
+        (PieceLength l) =
+            pieceLength
+
+        (NumColumns c) =
+            numColumns
+    in
+        (l * c)
+
+
+height : Properties -> Int
+height { numRows, pieceLength } =
+    let
+        (PieceLength l) =
+            pieceLength
+
+        (NumRows r) =
+            numRows
+    in
+        (l * r)
