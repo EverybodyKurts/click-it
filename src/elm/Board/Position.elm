6dd499a7b32895a1ee728049a5dc58df72420317
Kurt Mueller <kurtronaldmueller@gmail.com>
2017-10-16 20:55:28 -0400

    Move position functions into module

--- src/elm/Board/Position.elm
+++ src/elm/Board/Position.elm
@@ -1,5 +1,40 @@
-module Position exposing (..)
+module Board.Position exposing (..)
 
 
-a =
-    "b"
+type RowIndex
+    = RowIndex Int
+
+
+type ColumnIndex
+    = ColumnIndex Int
+
+
+type Position
+    = Position ( RowIndex, ColumnIndex )
+
+
+unwrapRow : Position -> Int
+unwrapRow (Position ( RowIndex r, _ )) =
+    r
+
+
+toTuple : Position -> ( Int, Int )
+toTuple (Position ( RowIndex r, ColumnIndex c )) =
+    ( r, c )
+
+
+sort : List Position -> List Position
+sort =
+    List.sortBy toTuple
+
+
+haveSameRow : Position -> Position -> Bool
+haveSameRow pos1 pos2 =
+    let
+        (Position ( RowIndex a, _ )) =
+            pos1
+
+        (Position ( RowIndex b, _ )) =
+            pos2
+    in
+        a == b
